a727290fbdeae54e353fe5bfbb8eb34e6ffe9b67
Nicolette Anderson <anderson.nicolette.r@gmail.com>
2023-06-09 19:48:25 -0500

    Add project files.

---
+++ FuelAppApi/Controllers/FuelController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace FuelAppApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FuelController : ControllerBase
+    {
+        public static string GetResponse(string endPoint)
+        {
+            HttpWebRequest request = CreateWebRequest(endPoint);
+
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                var responseValue = string.Empty;
+
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    string message = String.Format("POST failed. Received HTTP {0}", response.StatusCode);
+                    throw new ApplicationException(message);
+                }
+
+                // grab the response  
+                using (var responseStream = response.GetResponseStream())
+                {
+                    using (var reader = new StreamReader(responseStream))
+                    {
+                        responseValue = reader.ReadToEnd();
+                    }
+                }
+
+                return responseValue;
+            }
+        }
+
+        private static HttpWebRequest CreateWebRequest(string endPoint)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(endPoint);
+
+            request.Method = "GET";
+            request.ContentLength = 0;
+            request.ContentType = "text/json";
+
+            return request;
+        }
+    }
+}
---
+++ FuelAppUI/Pages/Index.cshtml.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FuelAppUI.Pages
+{
+    public class IndexModel : PageModel
+    {
+       
+    }
+}
